06781e653d45b67dbeeef4eb9a83184026afb8cb
marco987 <pacemarco87@gmail.com>
2019-07-11 14:31:12 +0200

    Creazione file

---
+++ script.js
@@ -0,0 +1,7 @@
+// CENSURATORE
+// Il software riceve in input un lungo testo
+// e una serie di parole da censurare.
+// Restituisce il testo con xxx
+// al posto delle parole censurate.
+// Stampa un “badword index” calcolato come
+// il numero di parole censurate sul numero di parole totali

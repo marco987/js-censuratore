c2cd0e753d06fcb035047dd639301d35e7452a74
marco987 <pacemarco87@gmail.com>
2019-07-12 08:37:08 +0200

    Numero di parole censurate sul totale

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 
 var testo = "La mia anima è pervasa da una mirabile serenità, simile a queste belle mattinate di maggio che io godo con tutto il cuore. Sono solo e mi rallegro di vivere in questo luogo che sembra esser creato per anime simili alla mia. Sono così felice, mio caro, così immerso nel sentimento della mia tranquilla esistenza che la mia arte ne soffre. Non potrei disegnare nulla ora, neppure un segno potrei tracciare; eppure mai sono stato così gran pittore come in questo momento. Quando l'amata valle intorno a me si avvolge nei suoi vapori, e l'alto sole posa sulla mia foresta";
 
-var parole_censurate = ["mia", "mio", "sono", "questo", "queste"];
+var parole_censurate = ["mia", "mio", "sono", "questo", "queste", "e"];
 
 console.log(testo.split(" "));
 var testo_split = testo.split(" ");
@@ -21,3 +21,19 @@ for (var i = 0; i < testo_split.length; i++) {
 }
 
 console.log(testo_split);
+
+var parole_totali = testo_split.length;
+
+console.log("Questo è il numero di parole totali: ", parole_totali);
+
+var contatore = 0;
+
+for (var i = 0; i < testo_split.length; i++) {
+  if (testo_split[i] == "XXX") {
+    contatore ++;
+  }
+}
+
+console.log("Questo è il numero di parole censurate: ", contatore);
+
+console.log("Sono state censurate ", contatore, " parole su ", parole_totali);
